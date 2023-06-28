bec9f292fbf77bfce06bf78bd0b2ac3f099a8847
takesi-dev <the.time.is.mony@gmail.com>
2023-06-29 01:20:30 +0900

    Json post

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::env;
 
@@ -10,7 +16,8 @@ async fn main() {
     tracing_subscriber::fmt::init();
     
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/users", post(create_user));
     let addr = SocketAddr::from(([127,0,0,1], 3000));
     tracing::debug!("listening on {}", addr);
 
@@ -22,4 +29,26 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn create_user (
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+    
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
